3b56e3dc8cf07b58405eb07898f07221f80b7e15
dennisdebel <dennisdebel@gmail.com>
2017-02-20 20:46:01 +0100

    more options for custom quill toolbar

--- static/dist/bundle.js
+++ static/dist/bundle.js
@@ -34,8 +34,27 @@ doc.fetch(function(err) {
 									       });
 
 
-var toolbarOptions = [['bold', 'italic'], ['link', 'image']]; // add custom toolbar to Quill editor
+// add custom toolbar to Quill editor
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['link','image','code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }], 
+//  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+//  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+//  [{ 'direction': 'rtl' }],                         // text direction
 
+//  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }], 
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+//  [{ 'font': [] }],
+    [{ 'align': [] }], 
+
+    ['clean']                                         // remove formatting button
+ ];  
+ 
 doc.subscribe(function(err) {
   if (err) throw err;
   var quill = new Quill('#editor', {
