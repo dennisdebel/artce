df8809f1515b2c3a6ff5fc5ec24d732f105dc9d8
dennisdebel <dennisdebel@gmail.com>
2017-03-06 14:12:09 +0100

    added auto-formatting urls
    urls become links
    image urls become inline images

--- static/dist/bundle.js
+++ static/dist/bundle.js
@@ -35,7 +35,6 @@ doc.fetch(function(err) {
 
 
 
-
 var toolbarOptions = [
 	['bold', 'italic', 'underline', 'strike'],        // toggled buttons
 	['link','image','code-block'],
@@ -68,6 +67,8 @@ doc.subscribe(function(err) {
 																		        theme: 'snow'
 																				    });
 
+  
+
 // render html for export, also look into jsPDF, make button:
 var exportButton = document.querySelector('.ql-export'); //export button 
 exportButton.addEventListener('click', function(e) {
@@ -98,6 +99,79 @@ function oneSecondFunction() {
 //var timer = setInterval(oneSecondFunction,1000); //check for newlines and count them
 
 quill.on('text-change', function(delta, oldDelta, source) {
+
+//custom matcher code from here:  https://github.com/quilljs/quill/issues/109
+
+var Delta = Quill.import('delta');
+
+
+//convert img links to actual images
+quill.clipboard.addMatcher(Node.TEXT_NODE, function(node, delta) {
+	var regex = /https?:\/\/[^\s]+/g;
+		if(typeof(node.data) !== 'string') return;
+			var matches = node.data.match(regex);
+
+			if(matches && matches.length > 0) {
+				var ops = [];
+				var str = node.data;
+				
+				matches.forEach(function(match) {
+					 var split = str.split(match);
+					 if(match.match(/\.(png|jpg|jpeg|gif)$/)!=null){
+					 
+					 var beforeLink = split.shift();
+					 ops.push({ insert: beforeLink });
+					 ops.push({ insert:{image: match}, attributes: { link: match } });
+																						                   str = split.join(match);
+					} else { //if link is not an image
+						var beforeLink = split.shift();
+					    ops.push({ insert: beforeLink });
+						ops.push({ insert: match, attributes: { link: match } });
+						str = split.join(match);											
+					}																	                });
+
+				ops.push({ insert: str });
+				delta.ops = ops;
+			 }                                                                                   return delta;
+});
+
+
+
+function isWhitespace(ch) {
+  var whiteSpace = false
+    if ((ch == ' ') || (ch == '\t') || (ch == '\n')) {
+	    whiteSpace = true;
+	 }
+		return whiteSpace;
+}
+
+
+//convert typed links to actual links, realtime
+quill.on('text-change', function(delta, oldDelta, source) {
+  var regex = /https?:\/\/[^\s]+$/;
+    if(delta.ops.length === 2 && delta.ops[0].retain && isWhitespace(delta.ops[1].insert)) {
+	    var endRetain = delta.ops[0].retain;
+		var text = quill.getText().substr(0, endRetain);
+		var match = text.match(regex);
+
+		if(match !== null) {
+			var url = match[0];
+			var ops = [];
+		
+		if(endRetain > url.length) {
+			ops.push({ retain: endRetain - url.length });
+		}
+
+		ops = ops.concat([
+			{ delete: url.length },
+			{ insert: url, attributes: { link: url } }
+		 ]);
+		
+		quill.updateContents({ops: ops													 });
+																					 }
+																					}
+																				});
+
 
  if (source !== 'user') return;
     doc.submitOp(delta, {source: quill});
